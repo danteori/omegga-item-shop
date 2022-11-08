02b7a449247f4aeac9963183ca006cdb12689b5b
Dante Stevens <danteorionstevens@gmail.com>
2022-11-08 15:55:14 -0500

    e

--- omegga.plugin.ts
+++ omegga.plugin.ts
@@ -22,7 +22,7 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
 
     this.omegga.on('cmd:itemplz', (speaker: string) => {
       const player = this.omegga.getPlayer(speaker);
-      player.giveItem('Weapon_Bazooka');
+      player.giveItem('Weapon_HeavyAssaultRifle');
     });
 
     /*
@@ -34,8 +34,38 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
 
     }
     */
+    this.omegga.on('cmd:buyitem', (speaker: string, subcommand: string) => {
+      const player = this.omegga.getPlayer(speaker);
+      if(!subcommand){
+        this.omegga.whisper(player, 'Usage: /buyitem # to buy item <color="cccccc">(example: /buyitem 1 to buy bazooka)</color>. Item numbers can be found using <color="00ff00">/itemshop</color>');
+      } else {
+        if(subcommand == '1'){
+          this.omegga.whisper(player, 'You have bought a <b>Bazooka</b> for <color="00ff00">$0</color>!')
+          player.giveItem('Weapon_Bazooka');
+        } else if(subcommand == '2'){
+          this.omegga.whisper(player, 'You have bought a <b>Heavy Assault Rifle</b> for <color="00ff00">$0</color>!')
+          player.giveItem('Weapon_HeavyAssaultRifle');
+        } else if(subcommand == '3'){
+          this.omegga.whisper(player, 'You have bought a <b>Bow</b> for <color="00ff00">$0</color>!')
+          player.giveItem('Weapon_Bow');
+        } else if(subcommand == '4'){
+          this.omegga.whisper(player, `You can't afford that shit, bitch. You currently have <color="00ff00">$0</color> in your account.`)
+        } else {
+          this.omegga.whisper(player, `What the hell is a ${subcommand}? Try again. Use /itemshop if you need help`)
+        }
+      }
+    });
 
-    return { registeredCommands: ['test', 'itemplz'] };
+    this.omegga.on('cmd:itemshop', (speaker: string) => {
+      const player = this.omegga.getPlayer(speaker);
+      this.omegga.whisper(player, `<b>iten shop</b> --- /buyitem # to buy item <color="cccccc">(example: /buyitem 1 to buy bazooka)</color>`,
+      '[1] - <b>Bazooka</b> - <color="00ff00">$0</color>',
+      '[2] - <b>Heavy Assault Rifle</b> - <color="00ff00">$0</color>',
+      '[3] - <b>Bow</b> - <color="00ff00">$0</color>',
+      '[4] - <color="ff00ff"><b>Mystery Item</b></color> - <color="00ff00">$1</color>');
+    });
+
+    return { registeredCommands: ['test', 'itemplz', 'itemshop', 'buyitem'] };
   }
 
   async stop() {
